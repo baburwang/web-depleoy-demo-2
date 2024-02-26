0a93f6c2be9e0dca98364167b10eef873016f205
baburwang <2551952143@qq.com>
2024-02-26 12:01:45 +0800

    feat: 调整脚本

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # 构建项目目录的路径
-PROJECT_DIR="${pwd}"
+PROJECT_DIR="$(pwd)"
 
 # 启动后端服务
 cd "${PROJECT_DIR}/server"
-npm install
-npm run start
+sudo npm install
+sudo npm run start
 
 # 前端资源打包
 cd "${PROJECT_DIR}/web"
-npm install
-npm run build
+sudo npm install
+sudo npm run build
 
 # 启动托管前端资源的服务
 cd "${PROJECT_DIR}/web/server"
-npm install
-npm run start
+sudo npm install
+sudo npm run start
 
 # 启动 nginx
 sudo nginx -s stop
